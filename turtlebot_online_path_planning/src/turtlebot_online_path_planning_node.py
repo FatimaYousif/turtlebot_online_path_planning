49c2b56068179fd3bc1b7e4a1bac4c5761e0260d
Fatima Yousif <49322171+FatimaYousif@users.noreply.github.com>
2024-03-12 22:07:43 +0100

    Add files via upload

---
+++ turtlebot_online_path_planning/src/turtlebot_online_path_planning_node.py
@@ -0,0 +1,294 @@
+#!/usr/bin/python3
+
+import numpy as np
+import rospy
+import tf
+
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+from std_msgs.msg import ColorRGBA 
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseStamped
+
+from utils_lib.online_planning import StateValidityChecker, move_to_point, compute_path
+
+class OnlinePlanner:
+
+    # OnlinePlanner Constructor
+    def __init__(self, gridmap_topic, odom_topic, cmd_vel_topic, bounds, distance_threshold):
+
+        # ATTRIBUTES
+        # List of points which define the plan. None if there is no plan
+        self.path = []
+        # State Validity Checker object                                                 
+        self.svc = StateValidityChecker(distance_threshold)
+        # Current robot SE2 pose [x, y, yaw], None if unknown            
+        self.current_pose = None
+        # Goal where the robot has to move, None if it is not set                                                                   
+        self.goal = None
+        # Last time a map was received (to avoid map update too often)                                                
+        self.last_map_time = rospy.Time.now()
+        # Dominion [min_x_y, max_x_y] in which the path planner will sample configurations                           
+        self.bounds = bounds                                        
+
+        # CONTROLLER PARAMETERS
+        # Proportional linear velocity controller gain
+        self.Kv = 2
+        # Proportional angular velocity controller gain                   
+        self.Kw = 3
+        # Maximum linear velocity control action                   
+        self.v_max = 0.15
+        # Maximum angular velocity control action               
+        self.w_max = 0.3       
+
+        self.max_retries = 10   # Maximum number of retries
+        self.max_planning_time = 10 # Initial planning time limit (s)
+        self.reserved_time = 10   #  planning time to increse after fail (s)
+      
+        # Recovery Behavior Parmeters 
+        self.recovery_time = rospy.Duration(5)
+        self.recovery_vel = -0.1
+
+        #--------------------ROS NODE
+        # PUBLISHERS
+        # Publisher for sending velocity commands to the robot
+        self.cmd_pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size=1) # TODO: publisher to cmd_vel_topic
+        # Publisher for visualizing the path to with rviz
+        self.marker_pub = rospy.Publisher('~path_marker', Marker, queue_size=1)
+        
+        # SUBSCRIBERS
+        self.gridmap_sub = rospy.Subscriber(gridmap_topic, OccupancyGrid, self.get_gridmap) # TODO: subscriber to gridmap_topic from Octomap Server  
+        self.odom_sub = rospy.Subscriber(odom_topic, Odometry, self.get_odom) # TODO: subscriber to odom_topic  
+        self.move_goal_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.get_goal) # TODO: subscriber to /move_base_simple/goal published by rviz    
+        
+
+        # TIMERS
+        # Timer for velocity controller
+        rospy.Timer(rospy.Duration(0.1), self.controller)
+    
+    # Odometry callback: Gets current robot pose and stores it into self.current_pose
+    def get_odom(self, odom):
+        _, _, yaw = tf.transformations.euler_from_quaternion([odom.pose.pose.orientation.x, 
+                                                              odom.pose.pose.orientation.y,
+                                                              odom.pose.pose.orientation.z,
+                                                              odom.pose.pose.orientation.w])
+
+        # TODO: Store current position (x, y, yaw) as a np.array in self.current_pose var.
+        self.current_pose =  np.array([odom.pose.pose.position.x, odom.pose.pose.position.y, yaw]) 
+        # ----- WE CAN ADD VELOCITY = FOR CLOSED LOOP
+    
+    # Goal callback: Get new goal from /move_base_simple/goal topic published by rviz 
+    # and computes a plan to it using self.plan() method
+    def get_goal(self, goal):
+        if self.svc.there_is_map:
+            # TODO: Store goal (x,y) as a numpy aray in self.goal var and print it 
+            self.goal = np.array([goal.pose.position.x, goal.pose.position.y])
+            print("Received new goal:", self.goal)
+            
+            if self.svc.is_valid(self.goal):
+                # Plan a new path to self.goal
+                self.plan()
+
+            else:
+                print("Goal is not valid")
+        
+    # Map callback: Gets the latest occupancy map published by Octomap server and update 
+    # the state validity checker
+    def get_gridmap(self, gridmap):
+      
+        # To avoid map update too often (change value '1' if necessary)
+        if (gridmap.header.stamp - self.last_map_time).to_sec() > 1:            
+            self.last_map_time = gridmap.header.stamp
+
+            # Update State Validity Checker
+            env = np.array(gridmap.data).reshape(gridmap.info.height, gridmap.info.width).T
+            origin = [gridmap.info.origin.position.x, gridmap.info.origin.position.y]
+            self.svc.set(env, gridmap.info.resolution, origin)
+
+            # If the robot is following a path, check if it is still valid
+            if self.path is not None and len(self.path) > 0:
+
+                # create total_path adding the current position to the rest of waypoints in the path
+                total_path = [self.current_pose[0:2]] + self.path
+
+                # TODO: check total_path validity. If total_path is not valid replan
+                
+                if not self.svc.check_path(total_path):
+                    rospy.logerr("Current path is not valid -- plan a new path")
+                    self.path = []
+                    self.plan()
+                else:
+                    rospy.logwarn("Current path is valid -- stay on same path")
+
+
+    # Solve plan from current position to self.goal. 
+    def plan(self):
+        # Invalidate previous plan if available
+        self.path = []
+
+        # Check if robot stuck in the obstacle 
+        while self.is_stuck_in_obst():
+            self.recover()
+            self.path = compute_path(start_p= self.current_pose[0:2],goal_p=self.goal[0:2],
+                                 state_validity_checker=self.svc, bounds= self.bounds,max_time=self.max_planning_time)
+        
+        
+        #print("Compute new path")
+        # TODO: plan a path from self.current_pose to self.goal
+        self.path = compute_path(start_p= self.current_pose[0:2],goal_p=self.goal[0:2],
+                                 state_validity_checker=self.svc, bounds= self.bounds,max_time=self.max_planning_time)
+        
+        # TODO: If planning fails, consider increasing the planning time, retry the planning a few times, etc.
+        if len(self.path) == 0:
+            for i in range(self.max_retries):
+                #rospy.logwarn("Path not found! --> Retrying...")
+                self.path = compute_path(start_p= self.current_pose[0:2],goal_p=self.goal[0:2],
+                                    state_validity_checker=self.svc, bounds= self.bounds, max_time=self.max_planning_time+ self.reserved_time) 
+                if self.path:
+                    break
+        
+        if len(self.path) == 0:
+            print("Path not found!")
+        else:
+            print("Path found")
+            # Publish plan marker to visualize in rviz
+            self.publish_path()
+            # remove initial waypoint in the path (current pose is already reached)= WANTING TO MOVE NEXT POSITION
+            del self.path[0]     
+
+        return self.path       
+ 
+    # This method is called every 0.1s. It computes the velocity comands in order to reach the 
+    # next waypoint in the path. It also sends zero velocity commands if there is no active path.
+
+    def controller(self, event):
+        v = 0
+        w = 0
+        if len(self.path) > 0:
+
+            # TODO: If current waypoint is reached with some tolerance (0.1) move to the next waypoint.
+            if np.linalg.norm(self.path[0] - self.current_pose[0:2]) < 0.1:  
+
+                del self.path[0]
+
+                # If it was the last waypoint in the path show a message indicating it 
+                
+                # or ?  rospy.loginfo("Waypoint reached --> Move to the next waypoint")
+                
+                if len(self.path) == 0:
+                    self.goal = None
+                    print("Final position reached!")
+                    
+            else: # TODO: Compute velocities using controller function in utils_lib (online_planning)
+                
+                # if self.curved_coltroller:
+                #     v,w = move_to_point_smooth(self.current_pose, self.path[0], Kp=10, Ki=10, Kd=10, dt=0.05)
+                # else:
+
+                v,w = move_to_point(self.current_pose, self.path[0], self.Kv, self.Kw)
+        else:
+            pass
+        # # Publish velocity commands
+        self.__send_commnd__(v, w)
+
+
+    def recover(self):
+        """
+        Recovery behavior by just moving the robot backward
+        """
+        rospy.logwarn("Robot is stuck in an obstacle! Recovering...")
+        cmd = Twist()
+        start_time = rospy.Time.now()
+        while (rospy.Time.now() -start_time ) < self.recovery_time:
+            v = self.recovery_vel
+            w = 0
+            cmd.linear.x = np.clip(v, -self.v_max, self.v_max)
+            cmd.linear.y = 0
+            cmd.linear.z = 0
+            cmd.angular.x = 0
+            cmd.angular.y = 0
+            cmd.angular.z = np.clip(w, -self.w_max, self.w_max)
+            self.cmd_pub.publish(cmd)
+        rospy.loginfo("Finished recovering!")
+        cmd.linear.x = 0
+        cmd.linear.y = 0
+        self.cmd_pub.publish(cmd)
+
+
+    def is_stuck_in_obst(self):
+        print(self.current_pose[0:2])
+        return not self.svc.is_valid(self.current_pose[0:2])
+
+    # PUBLISHER HELPERS = IMPORTANT TO UNDERSTAND - TEST - 14 MARCH EXPECTED 
+    # Transform linear and angular velocity (v, w) into a Twist message and publish it
+    def __send_commnd__(self, v, w):
+        cmd = Twist()
+        cmd.linear.x = np.clip(v, -self.v_max, self.v_max)
+        cmd.linear.y = 0
+        cmd.linear.z = 0
+        cmd.angular.x = 0
+        cmd.angular.y = 0
+        cmd.angular.z = np.clip(w, -self.w_max, self.w_max)
+        self.cmd_pub.publish(cmd)
+
+    # Publish a path as a series of line markers
+    def publish_path(self):
+        if len(self.path) > 1:
+            print("Publish path!")
+            m = Marker()
+            m.header.frame_id = 'odom'
+            m.header.stamp = rospy.Time.now()
+            m.id = 0
+            m.type = Marker.LINE_STRIP
+            m.ns = 'path'
+            m.action = Marker.DELETE
+            m.lifetime = rospy.Duration(0)
+            self.marker_pub.publish(m)
+
+            m.action = Marker.ADD
+            m.scale.x = 0.1
+            m.scale.y = 0.0
+            m.scale.z = 0.0
+            
+            m.pose.orientation.x = 0
+            m.pose.orientation.y = 0
+            m.pose.orientation.z = 0
+            m.pose.orientation.w = 1
+            
+            color_red = ColorRGBA()
+            color_red.r = 1
+            color_red.g = 0
+            color_red.b = 0
+            color_red.a = 1
+            color_blue = ColorRGBA()
+            color_blue.r = 0
+            color_blue.g = 0
+            color_blue.b = 1
+            color_blue.a = 1
+
+            p = Point()
+            p.x = self.current_pose[0]
+            p.y = self.current_pose[1]
+            p.z = 0.0
+            m.points.append(p)
+            m.colors.append(color_blue)
+            
+            for n in self.path:
+                p = Point()
+                p.x = n[0]
+                p.y = n[1]
+                p.z = 0.0
+                m.points.append(p)
+                m.colors.append(color_red)
+            
+            self.marker_pub.publish(m)
+            
+# MAIN FUNCTION
+if __name__ == '__main__':
+    rospy.init_node('turtlebot_online_path_planning_node')   
+    node = OnlinePlanner('/projected_map', '/odom', '/cmd_vel', np.array([-10.0, 10.0, -10.0, 10.0]), 0.2)
+    
+    # Run forever
+    rospy.spin()
---
+++ turtlebot_online_path_planning/src/utils_lib/online_planning.py
@@ -0,0 +1,273 @@
+import numpy as np
+import random
+import math
+
+def wrap_angle(angle):
+    return (angle + ( 2.0 * np.pi * np.floor( ( np.pi - angle ) / ( 2.0 * np.pi ) ) ) )
+
+# WORLD COORDINATES <<<<<<<<<<<< IMP
+# MAP = in SEM 1 - or - when u want to check if thereś an obstacle next
+
+class StateValidityChecker:
+    """ Checks if a position or a path is valid given an occupancy map."""
+
+    # Constructor
+    def __init__(self, distance=0.3, is_unknown_valid=True):
+        # map: 2D array of integers which categorizes world occupancy
+        self.map = None 
+        # map sampling resolution (size of a cell))                            
+        self.resolution = None
+        # world position of cell (0, 0) in self.map                      
+        self.origin = None
+        # set method has been called                          
+        self.there_is_map = False
+        # radius arround the robot used to check occupancy of a given position                 
+        self.distance = distance                    
+        # if True, unknown space is considered valid
+        self.is_unknown_valid = is_unknown_valid    
+    
+    # Set occupancy map, its resolution and origin. 
+    def set(self, data, resolution, origin):
+        self.map = data
+        self.resolution = resolution
+        self.origin = np.array(origin)
+        self.there_is_map = True
+    
+    # Given a pose, returs true if the pose is not in collision and false othewise.
+    # checking_path=False ++++++++++++++++++++++++++++++++++++++++++++++++++
+    
+    def is_valid(self, pose, checking_path=False):
+             
+
+        # TODO: 1. convert world robot position to map coordinates using method __position_to_map__
+        # TODO: 2. check occupancy of the vicinity of a robot position (indicated by self.distance atribute). 
+        # Return True if free, False if occupied and self.is_unknown_valid if unknown. 
+        # If checked position is outside the map bounds consider it as unknown.
+
+        # 1.
+             grid_pose = self.__position_to_map__(pose)
+
+             if self.__in_map__ == False:
+                return False
+             
+             if grid_pose != []:
+
+                grid_pose = (int(round(grid_pose[0],0)),int(round(grid_pose[1],0)))
+                map_value = self.map[grid_pose]
+                
+                # 2. Return True if free, False if occupied and self.is_unknown_valid if unknown.
+                if map_value == 0:   # If free space, check vicinity as well
+                    return self.__check_vicinity__(grid_pose, self.distance,checking_path)
+                    # return True
+                elif map_value == -1 and self.is_unknown_valid == True:
+                    return self.__check_vicinity__(grid_pose, self.distance,checking_path)
+                    # return True
+                else: # if obstacle, or if its unknown and is_unknown_valid = False, return False
+                    return False
+             else:
+                return False
+
+
+    # Given a path, returs true if the path is not in collision and false othewise.
+          
+    def check_path(self, path, step_size=0.04):
+
+        # 1. TODO: Discretize the positions between 2 waypoints with an step_size = 2*self.distance
+        # 2. TODO: for each point check if `is_valid``. If only one element is not valid return False, otherwise True.
+
+        waypoints = []  # upsampled points
+
+        # 1. 
+        for i in range(len(path)-1):
+            p1, p2 = path[i], path[i+1]
+            
+            dist = math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
+            
+            num_steps = dist / step_size
+            num_steps= int(num_steps)
+            
+            for j in range(num_steps):
+                interpolation = float(j) / num_steps  #the interpolation value for each step to find the pt we are checking right now
+                x = p1[0] * (1-interpolation) + p2[0] * interpolation
+                y = p1[1] * (1-interpolation) + p2[1] * interpolation
+                waypoints.append((x,y))
+
+        # 2. 
+        for w in waypoints:
+            if self.is_valid(w,checking_path=True) == False:
+                return False
+        return True
+    
+    def __position_to_map__(self, p):
+
+        # TODO: convert world position to map coordinates.
+        mx = (p[0]-self.origin[0])/self.resolution 
+        my = (p[1]-self.origin[1])/self.resolution
+        if self.__in_map__([mx,my]):
+            return [mx,my]
+        return [] 
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++
+    def __in_map__(self, loc):
+        '''
+        loc: list of index [x,y]
+        returns True if location is in map and false otherwise
+        '''
+        [mx,my] = loc
+        if mx >= self.map.shape[0]-1 or my >= self.map.shape[1]-1 or mx < 0 or my < 0:
+            return False 
+        return True
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++
+    def __check_vicinity__(self, cell, distance, checking_path=False):
+        if checking_path:
+            distance = distance/2
+        discrete_distance = int(round(distance/self.resolution,0))
+        for r in range(cell[0]-discrete_distance, cell[0]+discrete_distance):
+            for c in range(cell[1]-discrete_distance, cell[1]+discrete_distance):
+                if self.__in_map__([r,c]):
+                    map_value = self.map[r,c]
+                    if map_value == 100 or (map_value == -1 and self.is_unknown_valid == False): # if obstacle, or if its unknown and is_unknown_valid = False, return False
+                        return False
+        return True
+
+
+# Define RRT class (you can take code from Autonopmous Systems course!)
+class RRT:
+    def  __init__(self, state_validity_checker, max_iterations=10000, delta_q=4, p_goal=0.2, dominion=[-10, 10, -10, 10]):
+        # define constructor ...
+        self.K = max_iterations
+        self.delta_q = delta_q
+        self.prob = p_goal
+        self.min_x = dominion[0]
+        self.max_x = dominion[1]
+        self.min_y = dominion[2]
+        self.max_y = dominion[3]
+        self.parent = {}
+        self.svc = state_validity_checker
+    
+    def compute_path(self, q_start, q_goal):
+        # Implement RRT algorithm.
+        # Use the state_validity_checker object to see if a position is valid or not.
+        G = [q_start]
+        parent = {}
+        for k in range(self.K):
+            qrand = self.create_random_point(self.prob, self.min_x, self.max_x, self.min_y, self.max_y, q_goal)
+            qnear = self.find_nearest_node(qrand, G)
+
+            while self.svc.is_valid(qnear) is False:
+                 qnear = self.find_nearest_node(qrand, G)
+
+            qnew = self.determine_new_node(qnear, qrand, self.delta_q)
+            
+            if self.svc.check_path([qnear,qnew], step_size=0.04):
+                G.append(qnew)
+                (self.parent)[tuple(qnew)] = tuple(qnear)
+                if qnew[0] == q_goal[0] and qnew[1] == q_goal[1]:
+                    path = self.construct_path(G, qnew)
+                    path = self.smooth_path(path)
+                    return path
+        return []
+
+
+    def smooth_path(self, path):
+        # Optionally, you can implement a finction to smooth the RRT path.
+        start_counter = 0
+        goal_counter = len(path) - 1
+
+        start = path[start_counter]
+        goal = path[goal_counter]
+        smooth_path = [goal]    # smoothing GOAL TO START
+        while goal[0] != start[0] or goal[1] != start[1]:
+            if self.svc.check_path([start,goal], step_size=0.04):
+                smooth_path.insert(0, start)
+                goal = path[start_counter]
+                start_counter = 0
+                start = path[start_counter]
+            else:
+                start_counter = start_counter + 1
+                start = path[start_counter]
+        return smooth_path
+    
+    # If you need any auxiliar method include it in this class
+
+    # QRAND 
+    def create_random_point(self, probability, xmin, xmax, ymin, ymax, goal_point):
+        if random.random() > probability:
+            random_point = random.randint(xmin, xmax), random.randint(ymin, ymax)
+        else: 
+            random_point = goal_point
+        return random_point
+    
+    # Function to find the nearest node in the graph to a given point
+    # QNEAR
+    def find_nearest_node(self, random_point, graph):
+        distances = [math.sqrt((random_point[0]-vertex[0])**2 + (random_point[1]-vertex[1])**2) for vertex in graph]
+        min_index = distances.index(min(distances))
+        return graph[min_index]
+    
+    # Function to determine a new node's position considering the step size
+    # QNEW
+    def determine_new_node(self, closest_node, random_point, step_size):
+        distance = math.sqrt((random_point[0]-closest_node[0])**2 + (random_point[1]-closest_node[1])**2)
+        if distance < step_size:
+            return random_point
+        return self.move_towards_point(step_size, closest_node, random_point)
+    
+    # Function to construct the path from start to goal
+    def construct_path(self, graph, current_node):
+        path = [list(current_node)]  # Start with the current_node as a list
+        while tuple(current_node) in self.parent:
+            current_node = self.parent[tuple(current_node)]
+            path.insert(0, list(current_node))  # Convert current_node to a list and insert at the beginning
+        return path
+
+    
+    # Function to move towards a point by a fixed distance
+    def move_towards_point(self, step_size, start_point, end_point):
+        if start_point[0] == end_point[0] and start_point[1] == end_point[1]:
+            return start_point
+        direction = (end_point[0] - start_point[0], end_point[1] - start_point[1])
+        direction_length = math.sqrt(direction[0]**2 + direction[1]**2)
+        normalized = (direction[0]/direction_length, direction[1]/direction_length)
+        return (math.floor(start_point[0] + step_size * normalized[0]), math.floor(start_point[1] + step_size * normalized[1]))
+
+
+
+# Planner: This function has to plan a path from start_p to goal_p. To check if a position is valid the 
+# StateValidityChecker class has to be used. The planning dominion must be specified as well as the maximum planning time.
+# The planner returns a path that is a list of poses ([x, y]).
+def compute_path(start_p, goal_p, state_validity_checker, bounds, max_time=1.0):
+
+    # TODO: Plan a path from start_p to goal_p inside bounds using the RRT and the 
+    # StateValidityChecker Objects previously defined.
+    # TODO: if solved, return a list with the [x, y] points in the solution path.
+    # example: [[x1, y1], [x2, y2], ...]
+    # TODO: Ensure that the path brings the robot to the goal (with a small tolerance)!
+    
+    rrt = RRT(state_validity_checker, dominion=bounds)
+    path = rrt.compute_path(start_p, goal_p)
+
+    if path:
+        return path
+    else:
+        return []
+
+
+# DONE = ROS CRASHCOURSE
+# Controller: Given the current position and the goal position, this function computes the desired 
+# lineal velocity and angular velocity to be applied in order to reah the goal. = DD robot
+def move_to_point(current, goal, Kv=0.5, Kw=0.5):
+    
+    # TODO: Use a proportional controller which sets a velocity command to move from current position to goal (u = Ke)
+    # To avoid strange curves, first correct the orientation and then the distance. 
+    # Hint: use wrap_angle function to maintain yaw in [-pi, pi]
+    # This function should return only  linear velocity (v) and angular velocity (w)
+
+    d = math.sqrt((goal[0] - current[0])**2 + (goal[1] - current[1])**2)
+    psi_d = math.atan2(goal[1] - current[1], goal[0] - current[0])
+    w = Kw * wrap_angle(psi_d - current[2])
+    v = Kv* d
+    if wrap_angle(psi_d - current[2]) > 0.03:
+        v = 0
+    return v, w
